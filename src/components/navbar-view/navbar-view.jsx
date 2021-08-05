3efa1872f01b3b08366409982ecb8ef15835f7d9
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-08-05 15:35:12 +0200

    import logo and design for navbar

--- src/components/navbar-view/navbar-view.jsx
+++ src/components/navbar-view/navbar-view.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
-
+import bukness from "../../../Public/img/Bukness-main.png"
+import './navbar-view.scss';
 export class NavbarView extends React.Component {
   render(){
     return (
     <Navbar bg="primary" variant="dark">
     <Container>
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Navbar.Brand href="/"><img className="logo-main" src={bukness}/></Navbar.Brand>
     <Nav className="me-auto">
       <Nav.Link href="#home">Home</Nav.Link>
       <Nav.Link href="#features">Features</Nav.Link>
